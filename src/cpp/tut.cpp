e202a6ff990d011e99d23bf4340196c79d324485
bryanedds <bryanedds@gmail.com>
2015-11-06 12:03:05 -0500

    Added std::endl for legibility.

--- src/cpp/tut.cpp
+++ src/cpp/tut.cpp
@@ -12,22 +12,22 @@ int main(int, char*[])
 {
     /// leveraging our data abstraction
     tut::data_abstraction da(10);
-    std::cout << tut::func(da);
-    std::cout << tut::func_ex(da);
+    std::cout << tut::func(da) << std::endl;
+    std::cout << tut::func_ex(da) << std::endl;
 
     /// we don't need std::bind for stand-alone functions - they're much easier to pass around than
     /// member functions.
     const auto fp(&tut::func);
-    std::cout << fp(da);
+    std::cout << fp(da) << std::endl;
 
     /// leveraging structural polymorphism
-    std::cout << tut::pow(da);
-    std::cout << tut::pow_ex(da);
+    std::cout << tut::pow(da) << std::endl;
+    std::cout << tut::pow_ex(da) << std::endl;
 
     /// leveraging static polymorphism
-    std::cout << tut::op(3, 5);
-    std::cout << tut::op(3.0f, 5.0f);
-    std::cout << tut::op('3', '5');
+    std::cout << tut::op(3, 5) << std::endl;
+    std::cout << tut::op(3.0f, 5.0f) << std::endl;
+    std::cout << tut::op('3', '5') << std::endl;
 
     /// make a widget, and cast it back and forth
     auto widget = std::make_unique<tut::widget>(12345, 101.0f, true);
@@ -35,7 +35,7 @@ int main(int, char*[])
     auto widget_again = xtd::cast_unique<tut::widget>(std::move(castable));
 
     /// check its replacability :)
-    std::cout << tut::should_replace_with(*widget_again, 100.0f, 12345);
+    std::cout << tut::should_replace_with(*widget_again, 100.0f, 12345) << std::endl;
     return 0;
 }
 
